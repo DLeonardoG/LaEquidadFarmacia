f816a7aa0623119be0782bb8fd9e3d9c9e82fe8b
Leonardo Gonzalez <darkleonardog@gmail.com>
2025-04-11 17:05:00 -0500

    feat: :art: Cors was added

---
+++ demo/src/main/java/com/somic/demo/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.somic.demo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // permite todas las rutas
+                        .allowedOrigins("*") // permite todos los orígenes
+                        .allowedMethods("*") // permite todos los métodos HTTP
+                        .allowedHeaders("*"); // permite todos los headers
+            }
+        };
+    }
+}
